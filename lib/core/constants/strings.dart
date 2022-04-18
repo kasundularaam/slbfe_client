868d1205171b47fcb0eee90aec127619837c0576
Kasun Dulara <kasundularaam@gmail.com>
2022-04-19 00:58:17 +0530

    address changed

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -4,4 +4,8 @@ class Strings {
 
   static const landingImage = "assets/images/landing.png";
   static const avatarImage = "assets/images/avatar.png";
+
+  static const String portAddress = "8002";
+  static const String serverAddress = "http://192.168.8.100";
+  static const String baseUrl = "$serverAddress:$portAddress";
 }
--- lib/data/data_providers/data_provider.dart
+++ lib/data/data_providers/data_provider.dart
@@ -1,21 +1,21 @@
+import 'package:slbfe_client/core/constants/strings.dart';
+
 class DataProvider {
   static String getSlbfeUserUrl({required String nic}) =>
-      "http://192.168.8.100:8002/api/Citizens/$nic";
+      "${Strings.baseUrl}/api/Citizens/$nic";
 
   static String verifyInfoUrl({required String nic}) =>
-      "http://192.168.8.100:8002/api/Citizens/VerifyInformation/$nic";
+      "${Strings.baseUrl}/api/Citizens/VerifyInformation/$nic";
 
   static String uploadDocsUrl({required String nic}) =>
-      "http://10.0.2.2:8002/api/Citizens/$nic";
+      "${Strings.baseUrl}/api/Citizens/$nic";
 
-  static String get newUserUrl => "http://192.168.8.100:8002/api/Citizens";
+  static String get newUserUrl => "${Strings.baseUrl}/api/Citizens";
 
-  static String get loginUserUrl =>
-      "http://192.168.8.100:8002/api/Citizens/LoginUser";
+  static String get loginUserUrl => "${Strings.baseUrl}/api/Citizens/LoginUser";
 
   static String getComplaintsUrl({required String uid}) =>
-      "http://192.168.8.100:8002/api/Complaint/GetByUserId/$uid";
+      "${Strings.baseUrl}/api/Complaint/GetByUserId/$uid";
 
-  static String get newComplaintUrl =>
-      "http://192.168.8.100:8002/api/Complaint";
+  static String get newComplaintUrl => "${Strings.baseUrl}/api/Complaint";
 }
